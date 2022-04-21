8f4ee92a4221d379591861f2edcefaa563e3d3e4
Jim Sellers <jim.sellers@gmail.com>
2022-04-21 16:19:25 -0400

    adding different versions of things that are using buffered IO

--- src/main/java/com/github/sellersj/javahashperf/Application.java
+++ src/main/java/com/github/sellersj/javahashperf/Application.java
@@ -1,5 +1,6 @@
 package com.github.sellersj.javahashperf;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -79,6 +80,7 @@ public class Application {
         int count = 1;
         for (File file : files) {
             System.out.println(String.format("Checking file %s of %s ", count++, numberOfFiles));
+            checkFileBuffered(file);
             checkFile(file);
         }
         long t2 = System.currentTimeMillis();
@@ -150,6 +152,26 @@ public class Application {
     }
 
     /**
+     * "Check" the file by creating a hash of it.
+     *
+     * @param file to generate a hash for
+     */
+    public static void checkFileBuffered(File file) {
+        long t1 = System.currentTimeMillis();
+        String hex = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            hex = checksumWithBufferedInput(file.getAbsolutePath(), md);
+
+        } catch (NoSuchAlgorithmException | IOException e) {
+            throw new RuntimeException("could not create a check the file", e);
+        }
+
+        long t2 = System.currentTimeMillis();
+        printTime("It took %s milliseconds to create a checksum using buffered I/O which was " + hex, t1, t2);
+    }
+
+    /**
      * adapted from https://www.baeldung.com/java-random-string
      *
      * @param targetStringLength string length
@@ -172,6 +194,27 @@ public class Application {
 
         // file hashing with DigestInputStream
         try (DigestInputStream dis = new DigestInputStream(new FileInputStream(filepath), md)) {
+            while (dis.read() != -1) {
+                ; // empty loop to clear the data
+            }
+            md = dis.getMessageDigest();
+        }
+
+        // bytes to hex
+        StringBuilder result = new StringBuilder();
+        for (byte b : md.digest()) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString().toUpperCase(Locale.ROOT);
+
+    }
+
+    // from NistDataMirror
+    private static String checksumWithBufferedInput(String filepath, MessageDigest md) throws IOException {
+
+        // file hashing with DigestInputStream
+        try (
+            DigestInputStream dis = new DigestInputStream(new BufferedInputStream(new FileInputStream(filepath)), md)) {
             while (dis.read() != -1) {
                 ; // empty loop to clear the data
             }
