c867351e7db89ea63539d1a14b78e4d1cb663cb0
Jim Sellers <jim.sellers@gmail.com>
2022-04-20 20:23:36 -0400

    starting to get the data setup done

--- src/main/java/com/github/sellersj/javahashperf/Application.java
+++ src/main/java/com/github/sellersj/javahashperf/Application.java
@@ -1,9 +1,50 @@
 package com.github.sellersj.javahashperf;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Application {
+
+    private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+    }
+
+    public static List<File> createRandomFiles(int size) {
+        List<File> result = new ArrayList<>();
+        System.out.println(String.format("About to create %s files", size));
+        long t1 = System.currentTimeMillis();
+
+        try {
+            for (int i = 0; i < size; i++) {
+                File file = File.createTempFile("temp-file-", ".data");
+                file.deleteOnExit();
+
+                // fill the file with junk here
+                PrintWriter writer = new PrintWriter(file, "UTF-8");
+                // TODO make this random data
+                writer.println("The first line");
+                writer.println("The second line");
+                writer.close();
+
+                result.add(file);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("could not create a random file", e);
+        }
+        long t2 = System.currentTimeMillis();
+        printTime("It took %s milliseconds to create the random files", t1, t2);
+
+        return result;
+    }
+
+    public static void printTime(String message, long t1, long t2) {
+        System.out.println(String.format(message, (t2 - t1)));
     }
 
 }
--- src/test/java/com/github/sellersj/javahashperf/ApplicationTest.java
+++ src/test/java/com/github/sellersj/javahashperf/ApplicationTest.java
@@ -1,6 +1,25 @@
 package com.github.sellersj.javahashperf;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 public class ApplicationTest {
+
+    @Test
+    public void createRandomFiles() {
+        int size = 2;
+        List<File> files = Application.createRandomFiles(size);
+        assertEquals(2, files.size());
+
+        for (File file : files) {
+            assertTrue(file.exists(), file.getAbsolutePath() + " should exist");
+            assertTrue(0 != file.length(), "file should have zero lenght but was " + file.length());
+        }
+    }
 
 }
